bef55efd57fb9806a998d89e51845d7b8ddf5f40
gabrielcalderonr <64187735+gabrielcalderonr@users.noreply.github.com>
2021-05-21 10:36:09 +0530

    Add files via upload

---
+++ lesson5/js/list.js
@@ -0,0 +1,10 @@
+      button.onclick = function() {
+        let myItem = input.value;
+        input.value = '';
+
+       istBtn.onclick = function(e) {
+          list.removeChild(listItem);
+        }
+
+        input.focus();
+      }
