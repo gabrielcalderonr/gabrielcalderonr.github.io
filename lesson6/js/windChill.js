ad8cc494cc558fff5a6b626a083d4e006bfb818d
gabrielcalderonr <64187735+gabrielcalderonr@users.noreply.github.com>
2021-05-30 11:16:26 +0530

    Add files via upload

---
+++ lesson6/js/windChill.js
@@ -0,0 +1,10 @@
+let t = parseFloat(document.getElementById("temp").innerHTML);
+let w = parseFloat(document.getElementById("speed").innerHTML);
+
+if (t >= 50 && w >= 3) {
+    let windChill = 35.74 + 0.6215 * t - 35.75 * w *.16 + .4275 * t * w * .16;
+    document.getElementById("windChill").innerText = parseInt(windChill);
+}
+else{
+    document.getElementById("windChill").innerText = "N/A";
+}
